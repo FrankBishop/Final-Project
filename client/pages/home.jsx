a7a98421f9df56b62431a856eaa8e02d0080c3c7
FrankBishop <ffbishop3@gmail.com>
2021-04-06 12:59:15 -0700

    images now showing up on mobile app

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -8,7 +8,15 @@ export default function Home(props) {
         <h1> {props.text} </h1>
       </header>
       <main>
-
+        <h1 className="main-header">Popular Shows</h1>
+        <div className="image-holder-row">
+          <img src="/images/683813.jpg" alt="The Mandalorian" />
+          <img src="/images/746929.jpg" alt="Superman and Lois" />
+        </div>
+        <div className="image-holder-row">
+          <img src="/images/739854.jpg" alt="The Falcon and The Winter Soldier" />
+          <img src="/images/679902.jpg" alt="The Simpsons" />
+        </div>
       </main>
       <footer>
 

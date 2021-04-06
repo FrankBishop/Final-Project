4d38c27368ff92e22bb65296ca0fa13e40c3d22f
FrankBishop <ffbishop3@gmail.com>
2021-04-06 12:02:24 -0700

    finished header for mobile devices

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -2,9 +2,17 @@ import React from 'react';
 
 export default function Home(props) {
   return (
-    <header>
-      <i className="fas fa-tv fa-2x tv-icon"></i>
-      <h1> {props.text} </h1>
-    </header>
+    <div>
+      <header>
+        <i className="fas fa-tv fa-2x tv-icon"></i>
+        <h1> {props.text} </h1>
+      </header>
+      <main>
+
+      </main>
+      <footer>
+
+      </footer>
+    </div>
   );
 }

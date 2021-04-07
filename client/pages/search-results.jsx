dbba71f63416175a6a26c34f2e4640373cd65cf6
FrankBishop <ffbishop3@gmail.com>
2021-04-07 10:39:42 -0700

    partially working search results

--- client/pages/search-results.jsx
+++ client/pages/search-results.jsx
@@ -7,38 +7,22 @@ class SearchResults extends React.Component {
   //   // this.state = { value: '', searching: false };
   // }
 
-
-
   render() {
-    const ListItem = <li></li>
-    console.log('rendered')
+    function ListItem(props) {
+      return <ul>{props.value}</ul>;
+    }
     const results = this.props.results;
     console.log(results);
     const listResults = results.map((result) =>
-      <ListItem key={result.show.id} value={result.show} />
+      <div key={result.show.id}>
+        <img src={result.show.image.medium} ></img>
+        <ListItem value={result.show.name} />
+        <ListItem value={result.show.status} />
+      </div>
     );
-    console.log(listResults)
     return <ul> {listResults} </ul>
   }
 
 }
 
 export default SearchResults;
-
-// function ListItem(props) {
-//   return <li>{props.value}</li>;
-// }
-
-// function PokemonList(props) {
-//   const pokedex = props.pokedex;
-//   const listItems = pokedex.map((pokemon) =>
-//     <ListItem key={pokemon.number} value={pokemon.name} />
-//   );
-//   return (
-//     <ul>
-//       {listItems}
-//     </ul>
-//   );
-// }
-
-//fix li react element 

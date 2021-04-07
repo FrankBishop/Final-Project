b27401a93b69246bbbe705a44ee810d756e0117c
FrankBishop <ffbishop3@gmail.com>
2021-04-07 15:44:19 -0700

    event handler added for ul elements with title

--- client/pages/search-results.jsx
+++ client/pages/search-results.jsx
@@ -3,18 +3,20 @@ import React from 'react';
 class SearchResults extends React.Component {
 
   render() {
-    function ListItem(props) {
-      return <ul>{props.value}</ul>;
-    }
     const results = this.props.results;
     const filteredResults = results.filter(result => result.show.image != null);
     const listResults = filteredResults.map(result =>
-      <div className="search-result" key={result.show.id}>
+      <div className="search-result" key={result.show.id} id={result.show.id}>
         <img src={result.show.image.medium} alt={result.show.name} ></img>
-        <ListItem value={result.show.name} />
+        <ul onClick={this.ShowInfo} value={result.show.name}> {result.show.name} </ul>
       </div>
     );
     return <ul className="list-results"> {listResults} </ul>;
+  }
+
+  ShowInfo(event) {
+    console.log('it clicks a show');
+    console.log(event.target);
   }
 }
 

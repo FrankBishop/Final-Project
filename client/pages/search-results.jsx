bad8f389cf3de888df7ffe72efcf4d9a65f63cf3
FrankBishop <ffbishop3@gmail.com>
2021-04-07 12:58:33 -0700

    filtered results to prevent errors for ones without images

--- client/pages/search-results.jsx
+++ client/pages/search-results.jsx
@@ -7,17 +7,16 @@ class SearchResults extends React.Component {
       return <ul>{props.value}</ul>;
     }
     const results = this.props.results;
-    console.log(results);
-    const listResults = results.map((result) =>
+    const filteredResults = results.filter(result => result.show.image != null);
+    const listResults = filteredResults.map((result) =>
       <div className="search-result" key={result.show.id}>
-        <img src={result.show.image.medium} ></img>
+        <img src={result.show.image.medium} alt={result.show.name} ></img>
         <ListItem value={result.show.name} />
         <ListItem value={result.show.premiered} />
       </div>
     );
-    return <ul className="list-results"> {listResults} </ul>
+    return <ul className="list-results"> {listResults} </ul>;
   }
-
 }
 
 export default SearchResults;

a4a271ba1fbb3031bf806ff20c4831530924f1b6
FrankBishop <ffbishop3@gmail.com>
2021-04-08 12:16:27 -0700

    episode list button added to mobile

--- client/pages/showinfo.jsx
+++ client/pages/showinfo.jsx
@@ -13,16 +13,16 @@ class ShowInfo extends React.Component {
     const cast = this.state.showName._embedded.cast;
     const filteredCast = cast.filter(result => result.person.image != null);
     const listResults = filteredCast.map(result =>
-      <div className="cast-member" key={result.person.id}>
+      <div className="cast-member" key={result.character.id}>
         <img className="cast-image" src={result.person.image.medium} alt={result.person.name} />
         <ul className="actor" value={result.person.name}> {result.person.name} </ul>
         <ul className="character" value={result.character.name}> {result.character.name} </ul>
       </div>
     );
-    return <div>
+    return <div className="show-info">
       <h1 className="show-header">{this.state.showName.name}</h1>
       <h4>Premiere Date - {this.state.showName.premiered}</h4>
-      <h4 className="episodes-link">View Episode List</h4>
+      <button className="episodes-link">Episode List</button>
       <img className="show-image" src={this.state.showName.image.medium} alt={this.state.showName.name} ></img>
       <h4>Summary</h4>
       <div className="summary"> {filteredSummary}</div>

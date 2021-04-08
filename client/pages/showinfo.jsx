0d151fffaa2f6e3a80ef1307abba58427ec68eac
FrankBishop <ffbishop3@gmail.com>
2021-04-07 18:07:48 -0700

    cast list for show info page now shows up

--- client/pages/showinfo.jsx
+++ client/pages/showinfo.jsx
@@ -5,25 +5,28 @@ class ShowInfo extends React.Component {
   constructor(props) {
     super(props);
     this.state = { showName: this.props.show };
-    // this.ShowDetails = this.ShowDetails.bind(this);
   }
 
   render() {
-    // this.ShowDetails();
-    // if (this.state.showName !== null) {
+    console.log(this.state.showName)
+    const cast = this.state.showName._embedded.cast;
+    const filteredCast = cast.filter(result => result.person.image != null);
+    const listResults = filteredCast.map(result =>
+      <div className="cast-member" key={result.person.id}>
+        {/* <img src={result.show.image.medium} alt={result.show.name} ></img> */}
+        <ul value={result.person.name}> {result.person.name} </ul>
+        <ul value={result.character.name}> {result.character.name} </ul>
+        <img src={result.person.image.medium} alt={result.person.name}/>
+      </div>
+    );
     return <div>
       <h1>{this.state.showName.name}</h1>
-    </div>
-    // } else {
-    //   return <h1>No Show Selected</h1>
-    // }
+      <img src={this.state.showName.image.medium} alt={this.state.showName.name} ></img>
+      <div className="summary">{this.state.showName.summary}</div>
+      <ul>{listResults}</ul>
+    </div>;
   }
 
-  // ShowDetails() {
-  //   fetch('http://api.tvmaze.com/shows/' + this.props.show + '?embed[]=episodes&embed[]=cast')
-  //     .then(response => response.json())
-  //     .then(result => this.setState({ showName: result }));
-  // }
 }
 
 export default ShowInfo;

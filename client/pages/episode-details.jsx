44a4894f51d863d87a64ab59264dd7be2f2c499b
FrankBishop <ffbishop3@gmail.com>
2021-04-09 11:37:00 -0700

    buttons added with CSS for mobile

--- client/pages/episode-details.jsx
+++ client/pages/episode-details.jsx
@@ -7,17 +7,15 @@ class EpisodeDetails extends React.Component {
       <h1 className="show-header">{this.props.episode._embedded.show.name}</h1>
       <h2>S{this.props.episode.season}E{this.props.episode.number}</h2>
       <h2>{this.props.episode.name}</h2>
-      <img src= {this.props.episode.image.medium} alt="episode"/>
+      <img src={this.props.episode.image.medium} alt="episode" />
       <div className="episode-summary">
         <p>{this.props.episode.summary}</p>
       </div>
-      {/* <h1 className="show-header">{this.props.showName.name}</h1>
-      <h4 className="premiere-date">Premiere Date - {this.state.showName.premiered}</h4>
-      <button className="episodes-link" onClick={this.EpisodeListings}>Episode List</button>
-      <img className="show-image" src={this.state.showName.image.medium} alt={this.state.showName.name} ></img>
-      <h3 className="summary-header">Summary</h3>
-      <div className="summary"> {filteredSummary}</div>
-      <ul className="cast-list">{listResults}</ul> */}
+      <div className="episode-button-container">
+        <button>Log</button>
+        <button>Need To Watch</button>
+        <button>Mark as Watched</button>
+      </div>
     </div>;
   }
 }

ee9f1a33376395f5bb667ed9848819d817392e6e
FrankBishop <ffbishop3@gmail.com>
2021-04-09 11:41:18 -0700

    summary is filtered of HTML tags

--- client/pages/episode-details.jsx
+++ client/pages/episode-details.jsx
@@ -3,13 +3,15 @@ import React from 'react';
 class EpisodeDetails extends React.Component {
 
   render() {
+    const summary = this.props.episode.summary;
+    const filteredSummary = summary.replace(/<[^>]+>/g, '');
     return <div className="episode-info">
       <h1 className="show-header">{this.props.episode._embedded.show.name}</h1>
       <h2>S{this.props.episode.season}E{this.props.episode.number}</h2>
       <h2>{this.props.episode.name}</h2>
       <img src={this.props.episode.image.medium} alt="episode" />
       <div className="episode-summary">
-        <p>{this.props.episode.summary}</p>
+        <p>{filteredSummary}</p>
       </div>
       <div className="episode-button-container">
         <button>Log</button>

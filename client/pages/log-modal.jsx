a0fa51799f741ec504ced5780e4b6d75fc85c439
FrankBishop <ffbishop3@gmail.com>
2021-04-15 13:21:26 -0700

    stars now working

--- client/pages/log-modal.jsx
+++ client/pages/log-modal.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import StarRatings from './star';
+import ReactStars from 'react-stars';
 
 class LogModal extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = { modalOpen: true };
+    this.state = { modalOpen: true, rating: null };
+    this.ratingChanged = this.ratingChanged.bind(this);
   }
 
   render() {
@@ -17,7 +18,8 @@ class LogModal extends React.Component {
         <div className="delete-modal">
           <h2>How do you rate this episode?</h2>
           <div className="star-container">
-            <StarRatings />
+            {/* <StarRatings onChange={this.ratingChanged}/> */}
+            <ReactStars count={5} onChange={this.ratingChanged} size={60} color2={'#ffd700'} value={this.state.rating} />
           </div>
           <div className="log-modal-buttons">
             <button>Save To Log</button>
@@ -26,6 +28,10 @@ class LogModal extends React.Component {
         </div>
       </div >;
     }
+  }
+
+  ratingChanged(newRating) {
+    this.setState({ rating: newRating });
   }
 }
 

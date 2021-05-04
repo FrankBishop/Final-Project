986038298bf41d94b9b02d35d7de225b76294216
FrankBishop <ffbishop3@gmail.com>
2021-05-03 20:55:08 -0700

    finished polish 4

--- client/pages/search.jsx
+++ client/pages/search.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 class SearchForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { value: '' };
+    this.state = { value: ''};
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
